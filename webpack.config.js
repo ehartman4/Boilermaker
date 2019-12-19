4bc91a9d0b826859aff17d506b47293b00101da9
Emma Hartman <emmajhartman@gmail.com>
2019-12-19 18:05:24 -0500

    creating react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // entry: './index.js', // assumes your entry point is the index.js in the root of your project folder
+  entry: '.client/index.js',
+  mode: 'development',
+  output: {
+    // path: __dirname, // assumes your bundle.js will also be in the root of your project folder
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
